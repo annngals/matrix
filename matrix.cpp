c26c691e94613e58bf41e99401548f9f83a117c2
annngals <57696371+annngals@users.noreply.github.com>
2020-06-10 19:34:24 +0800

    Add files via upload

---
+++ matrix.cpp
@@ -0,0 +1,19 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include "math.h"
+using namespace std;
+
+int main()
+{
+
+}
+
+int FastPow(int a, int n) {
+	if (n == 0)
+		return 1;
+	if (n % 2 == 1)
+		return a * FastPow(a, n - 1);
+	else
+		return FastPow(a ^ 2, n / 2);
+}
